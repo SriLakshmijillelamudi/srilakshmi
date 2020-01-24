419f13451223bd7b414e8a46603b9ae325e93a20
SriLakshmijillelamudi <60178678+SriLakshmijillelamudi@users.noreply.github.com>
2020-01-24 17:28:10 +0530

    python automation

---
+++ first.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+pyautogui.position()
+pyautogui.click(226,750)
+time.sleep(2)
+pyautogui.typewrite("https://tx22072.us-east-2.aws.snowflakecomputing.com/console#/data/databases ")
+pyautogui.typewrite(["ENTER"])
